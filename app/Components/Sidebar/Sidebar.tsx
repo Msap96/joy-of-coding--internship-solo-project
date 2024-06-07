bcd7228c6be9d4ca5d1b3f2ed475788b722721f9
Msap96 <100401083+Msap96@users.noreply.github.com>
2024-06-07 15:02:41 -0400

    added profile image

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -3,11 +3,19 @@
 import React from "react";
 import styled from "styled-components";
 import { useGlobalState } from "@/app/context/globalProvider";
+import Image from "next/image";
 
 function Sidebar() {
   const { theme } = useGlobalState();
 
-  return <SidebarStyled theme={theme}>Sidebar</SidebarStyled>;
+  return (
+    <SidebarStyled theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay"></div>
+        <Image width={70} height={70} src="/avatar1.png" alt="profile" />
+      </div>
+    </SidebarStyled>
+  );
 }
 
 const SidebarStyled = styled.nav`
