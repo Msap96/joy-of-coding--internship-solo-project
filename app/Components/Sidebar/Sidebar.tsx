25c03187351d3cf413e86356fefcc90b4739c308
Msap96 <100401083+Msap96@users.noreply.github.com>
2024-06-12 13:34:15 -0400

    Profile styling

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -6,9 +6,17 @@ import { useGlobalState } from "@/app/context/globalProvider";
 import Image from "next/image";
 import menu from "@/app/utils/menu";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 function Sidebar() {
   const { theme } = useGlobalState();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
 
   return (
     <SidebarStyled theme={theme}>
@@ -23,13 +31,23 @@ function Sidebar() {
         </h1>
       </div>
       <ul className="nav-items">
-        {menu.map((item, index) => (
-          <li key={index}>
-            {item.icon}
-            <Link href={item.link}>{item.title}</Link>
-          </li>
-        ))}
+        {menu.map((item) => {
+          const link = item.link;
+          return (
+            <li
+              key={item.id}
+              className={`nav-item ${pathname === link ? "active" : ""}`}
+              onClick={() => {
+                handleClick(link);
+              }}
+            >
+              {item.icon}
+              <Link href={link}>{item.title}</Link>
+            </li>
+          );
+        })}
       </ul>
+      <button></button>
     </SidebarStyled>
   );
 }
@@ -40,5 +58,26 @@ const SidebarStyled = styled.nav`
   background-color: ${(props) => props.theme.colorBg2};
   border: 2px solid ${(props) => props.theme.borderColor2};
   border-radius: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  color: ${(props) => props.theme.colorGrey3};
+
+  .profile {
+    margin: 1.5rem;
+    padding: 1rem 0.8rem;
+    position: relative;
+
+    border-radius: 1rem;
+    cursor: pointer;
+
+    font-weight: 500;
+    color: ${(props) => props.theme.colorGrey0};
+
+    display: flex;
+    align-items: center;
+
 `;
 export default Sidebar;
