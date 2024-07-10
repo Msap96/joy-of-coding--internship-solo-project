572c9b53bad74f0c819c98ea5f5e10b211403a8d
Msap96 <100401083+Msap96@users.noreply.github.com>
2024-07-10 17:47:03 -0400

    Creating Content

--- app/Components/Modal/CreateContent.tsx
+++ app/Components/Modal/CreateContent.tsx
@@ -8,7 +8,91 @@ function CreateContent() {
   const [completed, setCompleted] = useState(false);
   const [important, setImportant] = useState(false);
 
-  return <div>CreateContent</div>;
+  const handleChange = (name: string) => (e: any) => {
+    switch (name) {
+      case "title":
+        setTitle(e.target.value);
+        break;
+      case "description":
+        setDescription(e.target.value);
+        break;
+      case "date":
+        setDate(e.target.value);
+        break;
+      case "completed":
+        setCompleted(e.target.checked);
+        break;
+      case "important":
+        setImportant(e.target.checked);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div>
+      <h1>Create a Task</h1>
+      <div className="input-control">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          name="title"
+          onChange={handleChange("title")}
+          placeholder="e.g, Daily GitHub commit"
+        />
+      </div>
+      <div className="input-control">
+        <label htmlFor="description">Description</label>
+        <textarea
+          value={description}
+          onChange={handleChange("description")}
+          name="description"
+          id="description"
+          rows={4}
+          placeholder="e.g, Build an API"
+        ></textarea>
+      </div>
+      <div className="input-control">
+        <label htmlFor="date">Due Date</label>
+        <input
+          value={date}
+          onChange={handleChange("date")}
+          type="date"
+          name="date"
+          id="date"
+        />
+      </div>
+      <div className="input-control toggler">
+        <label htmlFor="completed">Toggle Completed</label>
+        <input
+          value={completed.toString()}
+          onChange={handleChange("completed")}
+          type="checkbox"
+          name="completed"
+          id="completed"
+        />
+      </div>
+      <div className="input-control toggler">
+        <label htmlFor="important">Toggle Important</label>
+        <input
+          value={important.toString()}
+          onChange={handleChange("important")}
+          type="checkbox"
+          name="important"
+          id="important"
+        />
+      </div>
+
+      <div className="submit-btn">
+        <button type="submit">
+          <span>Submit</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CreateContent;
